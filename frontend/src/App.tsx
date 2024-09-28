6f8390192c30ec0038313c1d941552c72e6be112
Martin Lenweiter <57860529+martin-lenweiter@users.noreply.github.com>
2024-09-28 03:04:45 +0200

    manifesto foundations

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,24 @@
 import React from 'react';
-import IdeaForm from './components/IdeaForm'; // Import the new form component
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { ThemeProvider } from '@mui/material/styles';
+import { CssBaseline } from '@mui/material';
+import theme from './theme';
+import Header from './components/Header';
+import IdeaForm from './components/IdeaForm';
+import Manifesto from './components/Manifesto';
 
 const App: React.FC = () => {
   return (
-    <div>
-      <IdeaForm />
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<IdeaForm />} />
+          <Route path="/manifesto" element={<Manifesto />} />
+        </Routes>
+      </Router>
+    </ThemeProvider>
   );
 };
 
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -6,19 +6,12 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-import { ThemeProvider } from '@mui/material/styles';
-import { CssBaseline } from '@mui/material';
-import theme from './theme'; // Assuming you have a theme file
-
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <App />
-    </ThemeProvider>
+    <App />
   </React.StrictMode>
 );
